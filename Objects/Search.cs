24fd5bc0542eb480ea6c853b55d4cb5b67448fb2
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-02-17 08:50:45 -0800

    add submission form and button with materalize styling

--- Objects/Search.cs
+++ Objects/Search.cs
@@ -4,6 +4,14 @@ namespace BDD.Objects
 {
     public class Search
     {
+        private string _toBeSearched;
+        private string _searchTerm;
+
+        public Search(string searchItem, string searchTerm)
+        {
+            _toBeSearched = searchItem;
+            _searchTerm = searchTerm;
+        }
         
     }
 }
