c0718b4c23a330e23f0588d6d1d3cc097447a824
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-02-17 10:19:26 -0800

    refactor backend code for sentence checker and create multi word match test for sentence checker debugging now

--- Objects/Search.cs
+++ Objects/Search.cs
@@ -65,34 +65,10 @@ namespace BDD.Objects
                 return false;
             }
         }
+
+        public int GetCounterCount()
+        {
+            return _matchCounter;
+        }
     }
 }
-
-
-// public bool SentenceChecker()
-// {
-//
-//     string[] wordArray = _toBeSearched.Split(null);
-//     if (wordArray.Length < 2)
-//     {
-//         return DoesItMatch(_toBeSearched, _searchTerm);
-//     }
-//     else
-//     {
-//         for(int i=0;i<wordArray.Length;i++)
-//         {
-//             if(DoesItMatch(wordArray[i], _searchTerm))
-//             {
-//                 return DoesItMatch(wordArray[i], _searchTerm);
-//             }
-//             else
-//             {
-//                 continue;
-//             }
-//         }
-//         if(_matchCounter == 0)
-//         {
-//             return false;
-//         }
-//     }
-// }
